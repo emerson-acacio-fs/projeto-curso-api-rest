82eb263a99c7dbccbf0bce2c0d0340aa9cab5290
emerson_acacio@lccv.ufal.br <emerson_acacio@lccv.ufal.br>
2022-03-20 22:41:36 -0300

    criando as primeiras migrations

---
+++ src/shared/typeorm/migrations/1647824369429-CreateProducts.ts
@@ -0,0 +1,7 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+export class CreateProducts1647824369429 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {}
+
+  public async down(queryRunner: QueryRunner): Promise<void> {}
+}
