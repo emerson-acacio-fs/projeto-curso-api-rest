c26ef344063b672e3f0a7bff86261e58ac67f57c
emerson_acacio@lccv.ufal.br <emerson_acacio@lccv.ufal.br>
2022-03-28 22:21:09 -0300

    concluindo os controllers

---
+++ src/modules/products/services/DeleteProductService.ts
@@ -0,0 +1,19 @@
+import { AppError } from 'shared/errors/AppError';
+import { getCustomRepository } from 'typeorm';
+import { ProductRepository } from '../typeorm/repositories/ProductRepository';
+
+interface IRequest {
+  id: string;
+}
+
+export default class DeleteProductService {
+  public async execute({ id }: IRequest): Promise<void> {
+    const productRepository = getCustomRepository(ProductRepository);
+    const product = await productRepository.findOne({ id });
+    if (!product) {
+      throw new AppError('Product not found.');
+    }
+
+    await productRepository.remove(product);
+  }
+}
---
+++ src/modules/products/services/UpdateProductService.ts
@@ -0,0 +1,39 @@
+import { AppError } from 'shared/errors/AppError';
+import { getCustomRepository } from 'typeorm';
+import Product from '../typeorm/entities/Product';
+import { ProductRepository } from '../typeorm/repositories/ProductRepository';
+
+interface IRequest {
+  id: string;
+  name: string;
+  price: number;
+  quantity: number;
+}
+
+export default class UpdateProductService {
+  public async execute({
+    id,
+    name,
+    price,
+    quantity,
+  }: IRequest): Promise<Product> {
+    const productRepository = getCustomRepository(ProductRepository);
+
+    let product = await productRepository.findOne({ id });
+    if (!product) {
+      throw new AppError('Product not found.');
+    }
+
+    const productExists = await productRepository.findByName(name);
+
+    if (productExists && name !== product.name) {
+      throw new AppError('There is already a product with this name :(');
+    }
+
+    product = { ...product, name, price, quantity };
+
+    await productRepository.save(product);
+
+    return product;
+  }
+}
